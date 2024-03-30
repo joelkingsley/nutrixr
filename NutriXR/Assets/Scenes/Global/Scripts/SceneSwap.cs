ceaa722138dd79d9236f83abc9dc065b3ba4c67e
Fabian Schneider <fabsch@mail.uni-paderborn.de>
2024-03-30 14:59:35 +0100

    sync

--- NutriXR/Assets/Scenes/Global/Scripts/SceneSwap.cs
+++ NutriXR/Assets/Scenes/Global/Scripts/SceneSwap.cs
@@ -22,7 +22,7 @@ public class SceneSwap : MonoBehaviour
 
             if (currentScene.name == "Supermarket")
             {
-                SceneManager.LoadScene("Scenes/Kitchen");
+                SceneManager.LoadScene("Personal Workspace/fabsch/Kitchen");
             }
         }
     }
