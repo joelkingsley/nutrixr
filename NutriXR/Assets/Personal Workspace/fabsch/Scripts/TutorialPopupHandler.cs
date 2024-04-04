5aac4083fcf154e151b4156fc834866144632c78
Fabian Schneider <>
2024-04-04 20:15:10 +0200

    Added chained popups to supermarket cart

--- NutriXR/Assets/Personal Workspace/fabsch/Scripts/TutorialPopupHandler.cs
+++ NutriXR/Assets/Personal Workspace/fabsch/Scripts/TutorialPopupHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TutorialPopupHandler : MonoBehaviour
 {
+    [SerializeField] public GameObject nextPopup;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +21,9 @@ public class TutorialPopupHandler : MonoBehaviour
     public void HideElement()
     {
         gameObject.SetActive(false);
+        if (nextPopup != null)
+        {
+            nextPopup.SetActive(true);
+        }
     }
 }
