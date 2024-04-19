3b605ce8da0e182b026cf215c24fd45f7a8034a3
Arpita <arpitap@campus.uni-paderborn.de>
2024-04-19 18:39:45 +0200

    Nutri score calculation in RecipeCalculatedData.cs

--- NutriXR/Assets/Personal Workspace/neha2022/Scripts/RecipeCalculatedData.cs
+++ NutriXR/Assets/Personal Workspace/neha2022/Scripts/RecipeCalculatedData.cs
@@ -14,7 +14,15 @@ public class RecipeCalculatedData
     public float Protein ;
     public float CaloriesInKcal ;
     public float Calories ;
-    public  RecipeCalculatedData(RecipeItemData recipeItemData, List<IngredientItemData> ingredientItemDataList)
+    public char Nutriscorevalue;
+
+
+    // Define positive and negative points and nutriscore
+    float positivePoints = 0;
+    float negativePoints = 0;
+    float nutriScore= 0 ;
+
+    public RecipeCalculatedData(RecipeItemData recipeItemData, List<IngredientItemData> ingredientItemDataList)
     {
          RecipeName = recipeItemData.name;
          RecipeId = recipeItemData.id;
@@ -37,8 +45,49 @@ public class RecipeCalculatedData
 
                 }
             }
+
+            // Calculate negative points for high-quality nutrients
+            if (Protein > 0 && Carbohydrates > 0)
+            {
+                // Assuming the Nutri-Score ranges from 0 to -5 for high-quality nutrients
+                negativePoints -= 5; // Assigning maximum negative points for high-quality nutrients
+            }
+
+            // Calculate positive points for low-quality nutrients
+            if (Fat > 0 || Sugar > 0 || Calories> 0 || CaloriesInKcal > 0)
+            {
+                // Assuming the Nutri-Score ranges from 0 to 10 for low-quality nutrients
+                positivePoints += 10; // Assigning maximum positive points for low-quality nutrients
+            }
+
+            // Calculate total Nutri score
+             nutriScore = negativePoints - positivePoints;
+
+             // Assign letter grade based on Nutri score
+             if (nutriScore <= -15)
+             {
+                 Nutriscorevalue = 'A';
+             }
+             else if (nutriScore <= -10)
+             {
+                 Nutriscorevalue =  'B';
+             }
+             else if (nutriScore <= -5)
+             {
+                 Nutriscorevalue = 'C';
+             }
+             else if (nutriScore <= 0)
+             {
+                 Nutriscorevalue ='D';
+             }
+             else
+             {
+                 Nutriscorevalue='E';
+             }
         }
 
     }
 
 }
+
+
