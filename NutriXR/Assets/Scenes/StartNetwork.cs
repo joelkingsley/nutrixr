1753dac0146356323f2dbf4100c2e88dbe669390
Leonard Schmidt <leo1812@mail.uni-paderborn.de>
2024-04-27 17:56:46 +0200

    Added Avatar to Supermarket Scene + very basic multiplayer

---
+++ NutriXR/Assets/Scenes/StartNetwork.cs
@@ -0,0 +1,20 @@
+using Mirror;
+using UnityEngine;
+public class StartNetwork : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        #if UNITY_EDITOR
+        GetComponent<NetworkManager>().StartServer();
+        #else
+        GetComponent<NetworkManager>().StartClient();
+        #endif
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
