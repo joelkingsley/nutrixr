0f3500beb39ff5a9314a47dd1e13083d79912989
Julflips <104793554+Julflips@users.noreply.github.com>
2024-06-12 13:19:40 +0200

    Kitchen restructure + Eating Sound

--- NutriXR/Assets/Scenes/Kitchen/Scripts/CookedRecipe.cs
+++ NutriXR/Assets/Scenes/Kitchen/Scripts/CookedRecipe.cs
@@ -7,20 +7,36 @@ public class CookedRecipe : MonoBehaviour
 {
     private Transform userHead;
     private KitchenFeedbackSystem feedbackSystem;
+    private AudioSource audioSource;
+    private Rigidbody rb;
     private Recipe recipe;
+    public int destroyTimer ;
 
     private void Start()
     {
         userHead = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        audioSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
         feedbackSystem = GameObject.FindGameObjectWithTag("FeedbackSystem").GetComponent<KitchenFeedbackSystem>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if (Vector3.Distance(transform.position, userHead.position - new Vector3(0,0.1f,0)) < 0.1)
+        if (destroyTimer > 0)
+        {
+            destroyTimer--;
+            if (destroyTimer == 0)
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+        else if (Vector3.Distance(transform.position, userHead.position - new Vector3(0,0.1f,0)) < 0.15)
         {
             feedbackSystem.TrackConsumedRecipe(recipe);
-            Destroy(gameObject);
+            audioSource.Play();
+            rb.isKinematic = true;
+            destroyTimer = 69;
         }
     }
 
